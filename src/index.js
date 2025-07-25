60ee4b01c4b5219cce7f7e60c2aa587d0d2b6d42
ibrarli <ibraralihaidar@gmail.com>
2025-07-25 06:16:14 -0700

    update contact_list.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 module.exports.transaction_history= require('transaction_history')
+module.exports.contact_row = require('contact_row')
+module.exports.contacts_list = require('contacts_list')
+module.exports.search_bar = require('search_bar')
--- web/page.js
+++ web/page.js
@@ -2,20 +2,35 @@ const STATE = require('../src/node_modules/STATE')
 const statedb = STATE(__filename)
 const { sdb, get } = statedb(fallback_module)
 
-// const transactionHistory = require('../src/node_modules/transaction_history')
-// const transactionList = require('../src/node_modules/transaction_list')
-// const contactRow = require('../src/node_modules/contact_row')
-const contactsList = require('../src/node_modules/contacts_list')
-
-
+const contacts_list = require('../src/node_modules/contacts_list')
 
 const state = {}
 
-function protocol (message, notify) {
+function protocol(message, notify) {
   const { from } = message
   state[from] = { notify }
+
+  function listen(message) {
+    const { type, data } = message
+    if (on[type]) on[type](data)
+  }
+
   return listen
 }
+
+const on = {
+  style: injectStyle,
+  value: handleValue
+}
+
+function injectStyle (data) {
+  console.log('Injecting shared style (if needed)', data)
+}
+
+function handleValue (data) {
+  console.log(`"${data.id}" value:`, data.value)
+}
+
 
 function onbatch (batch) {
   console.log(' Watch triggered with batch:', batch)
@@ -33,25 +48,16 @@ async function main () {
 
   const subs = await sdb.watch(onbatch)
 
-
-//  const tHistoryComponent = await transactionHistory(subs[0], protocol)
-//  const tListComponent = await transactionList(subs[0], protocol)
-  // const contactRowComponent = await contactRow(subs[0], protocol)
-  const contactListComponent = await contactsList(subs[0], protocol)
+  const contact_list = await contacts_list(subs[0], protocol)
 
   const page = document.createElement('div')
   page.innerHTML = `
     <div >
-      <div id="history-container"></div>
-      <div id="list-container"></div>
       <div id="contacts-list-container" ></div>   
     </div>
   `
- 
-  // page.querySelector('#history-container').appendChild(tHistoryComponent)
-  // page.querySelector('#list-container').appendChild(tListComponent)
-  // page.querySelector('#contact-row-container').appendChild(contactRowComponent)
-  page.querySelector('#contacts-list-container').appendChild(contactListComponent)
+
+  page.querySelector('#contacts-list-container').appendChild(contact_list)
   document.body.append(page)
   console.log("Page mounted")
 }
@@ -111,127 +117,6 @@ function fallback_module () {
               data: 'data'
             }
           }
-
-
-
-      // '../src/node_modules/transaction_history': {
-      //   $: '',
-      //   0: {
-      //   value: [
-      //           {
-      //             date: "Today",
-      //             tid: "Luis fedrick",
-      //             ttime: "11:30 AM",
-      //             tamount: "+ 0.02456",
-      //             avatar: "https://tse4.mm.bing.net/th/id/OIP.VIRWK2jj8b2cHBaymZC5AgHaHa?w=800&h=800&rs=1&pid=ImgDetMain&o=7&rm=3"
-
-      //           },
-      //           {
-      //             date: "Today",
-      //             tid: "3TgmbHfn...455p",
-      //             ttime: "02:15 PM",
-      //             tamount: "+ 0.03271",
-      //             avatar: "https://tse4.mm.bing.net/th/id/OIP.VIRWK2jj8b2cHBaymZC5AgHaHa?w=800&h=800&rs=1&pid=ImgDetMain&o=7&rm=3"
-      //           },
-      //           {
-      //             date: "Today",
-      //             tid: "Mark Kevin",
-      //             ttime: "03:45 PM",
-      //             tamount: "- 0.00421",
-      //             avatar: "https://tse4.mm.bing.net/th/id/OIP.x-5S96eQh14_yvkqjsIOfwHaHa?w=800&h=800&rs=1&pid=ImgDetMain&o=7&rm=3"     
-      //           },
-      //           {
-      //             date: "Today",
-      //             tid: "7RwmbHfn...455p",
-      //             ttime: "04:45 PM",
-      //             tamount: "- 0.03791",
-      //             avatar: "https://tse2.mm.bing.net/th/id/OIP.7XLV6q-D_hA-GQh_eJu52AHaHa?rs=1&pid=ImgDetMain&o=7&rm=3"
-      //           },
-      //           {
-      //             date: "Yesterday",
-      //             tid: "Luis fedrick",
-      //             ttime: "11:30 AM",
-      //             tamount: "+ 0.02456",
-      //             avatar: "https://tse2.mm.bing.net/th/id/OIP.255ajP8y6dHwTTO8QbBzqwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3"
-      //           },
-      //           {
-      //             date: "Yesterday",
-      //             tid: "3TgmbHfn...455p",
-      //             ttime: "02:15 PM",
-      //             tamount: "+ 0.03271",
-      //             avatar: "https://tse4.mm.bing.net/th/id/OIP.x-5S96eQh14_yvkqjsIOfwHaHa?w=800&h=800&rs=1&pid=ImgDetMain&o=7&rm=3"     
-      //           },
-      //           {
-      //             date: "Yesterday",
-      //             tid: "Mark Kevin",
-      //             ttime: "03:45 PM",
-      //             tamount: "- 0.00421",
-      //             avatar: "https://tse4.mm.bing.net/th/id/OIP.bdn3Kne-OZLwGM8Uoq5-7gHaHa?w=512&h=512&rs=1&pid=ImgDetMain&o=7&rm=3"
-      //           },
-      //           {
-      //             date: "Yesterday",
-      //             tid: "7RwmbHfn...455p",
-      //             ttime: "04:45 PM",
-      //             tamount: "- 0.03791",
-      //             avatar: "https://tse4.mm.bing.net/th/id/OIP.VIRWK2jj8b2cHBaymZC5AgHaHa?w=800&h=800&rs=1&pid=ImgDetMain&o=7&rm=3"
-      //           },
-      //           {
-      //             date: "Dec 09",
-      //             tid: "Luis fedrick",
-      //             ttime: "11:30 AM",
-      //             tamount: "+ 0.02456",
-      //             avatar: "https://tse4.mm.bing.net/th/id/OIP.VIRWK2jj8b2cHBaymZC5AgHaHa?w=800&h=800&rs=1&pid=ImgDetMain&o=7&rm=3"
-      //           },
-      //           {
-      //             date: "Dec 09",
-      //             tid: "3TgmbHfn...455p",
-      //             ttime: "02:15 PM",
-      //             tamount: "+ 0.03271",
-      //             avatar: "https://tse4.mm.bing.net/th/id/OIP.x-5S96eQh14_yvkqjsIOfwHaHa?w=800&h=800&rs=1&pid=ImgDetMain&o=7&rm=3"
-      //           },
-      //           {
-      //             date: "Dec 09",
-      //             tid: "Mark Kevin",
-      //             ttime: "03:45 PM",
-      //             tamount: "- 0.00421",
-      //             avatar: "https://tse2.mm.bing.net/th/id/OIP.255ajP8y6dHwTTO8QbBzqwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3"
-      //           },
-      //           {
-      //             date: "Dec 09",
-      //             tid: "7RwmbHfn...455p",
-      //             ttime: "04:45 PM",
-      //             tamount: "- 0.03791",
-      //             avatar: "https://tse2.mm.bing.net/th/id/OIP.7XLV6q-D_hA-GQh_eJu52AHaHa?rs=1&pid=ImgDetMain&o=7&rm=3"
-      //           },
-      //         ]
-      //       },
-      //       mapping: {
-      //         style: 'style',
-      //         data: 'data'
-      //       }
-          
-      //     },
-          // '../src/node_modules/contact_row': {
-          // $: '',
-          // 0: {
-          // value: [
-          //         {
-          //           avatar: "https://tse4.mm.bing.net/th/id/OIP.VIRWK2jj8b2cHBaymZC5AgHaHa?w=800&h=800&rs=1&pid=ImgDetMain&o=7&rm=3",
-          //           name: 'Mark kevin',
-          //           message: 'Payment Re...',
-          //           time: '3 hr',
-          //           unread: 5,
-          //           online: false,
-          //           lightining: true
-          //         }
-          //       ]
-          //     },
-          //     mapping: {
-          //       style: 'style',
-          //       data: 'data'
-          //     }
-            
-          //   },
        }
     }
 }
