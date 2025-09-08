f2ef3199e21483765b96fb73eeaeac85749cfe8e
Ibrar-Haidar <106898965+Ibrar-Haidar@users.noreply.github.com>
2025-09-08 11:35:53 +0500

    Create more_menu component

--- web/page.js
+++ web/page.js
@@ -13,6 +13,8 @@ const transaction_receipt = require('../src/node_modules/transaction_receipt')
 const home_page = require('../src/node_modules/home_page')
 const lightning_page = require('../src/node_modules/lightning_page')
 const btc_nodes = require('../src/node_modules/btc_nodes')
+const more_menu = require('../src/node_modules/more_menu')
+
 
 document.title = 'flamingo wallet'
 document.head.querySelector('link').setAttribute('href', 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🦩</text></svg>')
@@ -72,6 +74,7 @@ async function main () {
   const receive_btc_component = await receive_btc(subs[14], protocol)
   const transaction_receipt_component = await transaction_receipt(subs[16], protocol)
   const btc_nodes_component = await btc_nodes(subs[18], protocol)
+  const more_menu_component = await more_menu(subs[20], protocol)
 
   const page = document.createElement('div')
   page.innerHTML = `
@@ -104,6 +107,10 @@ async function main () {
         <div class="component-label" style="padding-bottom:10px;">btc nodes</div>  
         <div id="btc-nodes-container"></div>
       </div>
+      <div style="font-size: 18px; font-weight: bold; font-family: Arial, sans-serif; margin-block: 10px;"> 
+        <div class="component-label" style="padding-bottom:10px;">More Menu</div>  
+        <div id="more-menu-container"></div>
+      </div>
     </div>
   `
   page.querySelector('#home-page-container').appendChild(home_page_component)
@@ -116,6 +123,7 @@ async function main () {
   page.querySelector('#receive-btc-container').appendChild(receive_btc_component)
   page.querySelector('#transaction-receipt-container').appendChild(transaction_receipt_component)
   page.querySelector('#btc-nodes-container').appendChild(btc_nodes_component)
+  page.querySelector('#more-menu-container').appendChild(more_menu_component)
 
   document.body.append(page)
   console.log("Page mounted")
@@ -355,19 +363,18 @@ function fallback_module () {
           icons: 'icons'
         }
       },
-        '../src/node_modules/btc_nodes': {
+      '../src/node_modules/btc_nodes': {
         $: '',
-        0: {
-        value: [
-              { label: "Sent By", value: "Cypher" },
-              { label: "Sent To", value: "Luis fedrick - 1FfmbHfn...455p" },
-              { label: "Time & Date", value: "30 June 2025, 09:32 AM" },
-              { label: "Transaction Fees", value: "BTC 0.0001" },
-              { label: "Recipient Receives", value: "BTC 0.0019" },
-              { label: "Blockchain Explorer", value: "https://mempool.space/tx/your_txid_here",  link: true },
-              { label: "Total Amount", value: "BTC 0.0020",  icon: "btc.svg" }
-            ]
-          },
+        0: '',
+        mapping: {
+          style: 'style',
+          data: 'data',
+          icons: 'icons'
+        }
+      },
+      '../src/node_modules/more_menu': {
+        $: '',
+        0: '',
         mapping: {
           style: 'style',
           data: 'data',
